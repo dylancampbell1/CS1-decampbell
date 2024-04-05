6af207d93e6bb61a7963e6472704083473ec76a1
Dylan Campbell <decampbell@mavs.coloradomesa.edu>
2024-04-04 20:12:32 -0600

    s

--- labs/L6/pointers.cpp
+++ labs/L6/pointers.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
     large_int *sum = new large_int; //declare sum as a dynamic variable
     large_int *prod = new large_int;
     large_int *diff = new large_int;
-    big_int *larger = new large_int;
+    big_int *larger = new big_int;
     // FIXME3 - declare dynamic variables to store difference and larger values *FIXED*
     // Must use these variables to store the returned values from functions
 
